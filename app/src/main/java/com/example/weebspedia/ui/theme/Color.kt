48c57cc0f01bace35ff040dbc3e0a9cefbc07b42
rakaagus <kakakunyukya@gmail.com>
2024-05-09 08:50:44 +0700

    Update: Update theme, color, typography

--- app/src/main/java/com/example/weebspedia/ui/theme/Color.kt
+++ app/src/main/java/com/example/weebspedia/ui/theme/Color.kt
@@ -6,6 +6,10 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+val primaryColor = Color(0xFF1A1D26)
+val secondaryColor = Color(0xFF222430)
+val colorInContainer = Color(0xFF3CD5FE)
+
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
--- app/src/main/java/com/example/weebspedia/ui/theme/Type.kt
+++ app/src/main/java/com/example/weebspedia/ui/theme/Type.kt
@@ -2,33 +2,40 @@ package com.example.weebspedia.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.weebspedia.R
 
-// Set of Material typography styles to start with
+val PoetOne = FontFamily(
+    Font(R.font.poetsenone_regular)
+)
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_regular),
+    Font(R.font.roboto_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    labelMedium = TextStyle(
+        fontSize = 14.sp,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = PoetOne
+    ),
+    labelLarge = TextStyle(
+        fontSize = 18.sp,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = PoetOne
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
 )
